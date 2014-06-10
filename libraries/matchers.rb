8cc44f00ee35cbc33707e30d74b78ad6234eeb28
Lucas Mariani <lmariani@lojack.com.ar>
2014-06-10 17:42:27 -0300

    correct matcher.rb

--- libraries/matchers.rb
+++ libraries/matchers.rb
@@ -1,51 +1,19 @@
 if defined?(ChefSpec)
 
-  def create_deploy_key_bitbucket(resource_name)
-    ChefSpec::Matchers::ResourceMatcher.new(:deploy_key_bitbucket, :create, resource_name)
+  def create_deploy_key(resource_name)
+    ChefSpec::Matchers::ResourceMatcher.new(:deploy_key, :create, resource_name)
   end
   
-  def delete_deploy_key_bitbucket(resource_name)
-    ChefSpec::Matchers::ResourceMatcher.new(:deploy_key_bitbucket, :delete, resource_name)
+  def delete_deploy_key(resource_name)
+    ChefSpec::Matchers::ResourceMatcher.new(:deploy_key, :delete, resource_name)
   end
 
-  def add_deploy_key_bitbucket(resource_name)
-    ChefSpec::Matchers::ResourceMatcher.new(:deploy_key_bitbucket, :add, resource_name)
+  def add_deploy_key(resource_name)
+    ChefSpec::Matchers::ResourceMatcher.new(:deploy_key, :add, resource_name)
   end
 
-  def remove_deploy_key_bitbucket(resource_name)
-    ChefSpec::Matchers::ResourceMatcher.new(:deploy_key_bitbucket, :remove, resource_name)
-  end
-
-  def create_deploy_key_github(resource_name)
-    ChefSpec::Matchers::ResourceMatcher.new(:deploy_key_github, :create, resource_name)
-  end
-  
-  def delete_deploy_key_github(resource_name)
-    ChefSpec::Matchers::ResourceMatcher.new(:deploy_key_github, :delete, resource_name)
-  end
-
-  def add_deploy_key_github(resource_name)
-    ChefSpec::Matchers::ResourceMatcher.new(:deploy_key_github, :add, resource_name)
-  end
-
-  def remove_deploy_key_github(resource_name)
-    ChefSpec::Matchers::ResourceMatcher.new(:deploy_key_github, :remove, resource_name)
-  end
-
-  def create_deploy_key_gitlab(resource_name)
-    ChefSpec::Matchers::ResourceMatcher.new(:deploy_key_gitlab, :create, resource_name)
-  end
-  
-  def delete_deploy_key_gitlab(resource_name)
-    ChefSpec::Matchers::ResourceMatcher.new(:deploy_key_gitlab, :delete, resource_name)
-  end
-
-  def add_deploy_key_gitlab(resource_name)
-    ChefSpec::Matchers::ResourceMatcher.new(:deploy_key_gitlab, :add, resource_name)
-  end
-
-  def remove_deploy_key_gitlab(resource_name)
-    ChefSpec::Matchers::ResourceMatcher.new(:deploy_key_gitlab, :remove, resource_name)
+  def remove_deploy_key(resource_name)
+    ChefSpec::Matchers::ResourceMatcher.new(:deploy_key, :remove, resource_name)
   end
 
 end
