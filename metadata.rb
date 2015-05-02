ff22b81a5831c0faf98366176f243f737d50dfd3
Cassiano Leal <cassianoleal@gmail.com>
2015-05-02 12:36:46 +0100

    Bump patch version for release

--- metadata.rb
+++ metadata.rb
@@ -4,4 +4,4 @@ maintainer_email "cl@cassianoleal.com"
 license          'Apache 2.0'
 description      'Manage deploy keys on Github and Bitbucket'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.2.0"
+version          "0.2.1"
